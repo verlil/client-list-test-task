e964b409877156401dac4ac1b4cb8c171a4b212f
verlil <vernigoralilia@gmail.com>
2018-09-18 10:07:02 +0300

    basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ClientDetailsComponent } from './client-details/client-details.component';
+import { ClientsListComponent } from './clients-list/clients-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientDetailsComponent,
+    ClientsListComponent
   ],
   imports: [
     BrowserModule
